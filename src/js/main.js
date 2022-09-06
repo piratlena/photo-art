46c7e64a468acbcf144423518f888ae511b1c4c9
Elena Brazhnikova <lenovosibirsk@mail.ru>
2022-09-06 18:57:47 +0700

    feat:implement sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import modals from "./modules/modals";
+import slider from './modules/sliders';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     modals();
+    slider('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    slider('.main-slider-item', 'vertical');
 })
